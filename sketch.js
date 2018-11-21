a965b87c5b114f4a8c57f61965b4d07ef096553d
Diogo Nunes <diogo_p_nunes@hotmail.com>
2018-11-21 11:08:17 +0100

    snow pile added

--- sketch.js
+++ sketch.js
@@ -1,13 +1,31 @@
+// snow
 var snowFlakes = [];
 var gravity;
 var wind;
 var angle;
+
+// pile of snow
+var pileSnow = [];
+var bottom;
 
 function setup() {
   createCanvas(600, 600);
   gravity = createVector(0, 0.1);
   wind = createVector(0.1, 0);
   angle = PI;
+  initPileSnow();
+}
+
+function initPileSnow() {
+  bottom = 30;
+  var h = 0;
+  for(var i = 1; i < width-1; i++) {
+    pileSnow[i] = map(sin(h), -1,1, 0, 10);
+    h += 0.1;
+  }
+
+  pileSnow[0] = bottom;
+  pileSnow[width-1] = bottom;
 }
 
 function draw() {
@@ -29,5 +47,15 @@ function draw() {
     flake.update();
     flake.render();
   }
+
+
+  // render pile of snow
+  fill(255);
+  translate(0, height - bottom);
+  beginShape();
+  for(var i = 0; i < width; i++) {
+    vertex(i, pileSnow[i]);
+  }
+  endShape();
 
 }
