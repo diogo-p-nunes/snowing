8c51c3a6f9b469d12b608da6e655d038ee820574
Diogo Nunes <diogo_p_nunes@hotmail.com>
2018-11-21 10:06:40 +0100

    snow falling

--- flake.js
+++ flake.js
@@ -1,14 +1,41 @@
 class Flake {
     constructor() {
         var x = random(width);
-        var y = random(height);
+        var y = random(-height, 0);
         this.pos = createVector(x, y)
         this.size = random(1, 10);
+        this.vel = createVector(0,0);
+    }
+
+    applyForce(acc) {
+        // v = v0 + force * t
+        this.vel.add(acc.mult(1));
     }
 
     render() {
         stroke(255);
         strokeWeight(this.size);
         point(this.pos.x, this.pos.y);
+    }
+
+    resetFlake() {
+        var x = random(width);
+        var y = random(-height, 0);
+        this.pos = createVector(x, y)
+        this.size = random(1, 10);
+        this.vel = createVector(0,0);
+    }
+
+    update() {
+        this.pos.add(this.vel);
+
+        // if pos is out of screen, reset flake
+        if(this.outOfScreen()) {
+            this.resetFlake();
+        }
+    }
+
+    outOfScreen() {
+        return this.pos.y > height || this.pos.x > width || this.pos.x < 0;
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,18 +1,32 @@
 var snowFlakes = [];
+var gravity;
+var wind;
 
 function setup() {
   createCanvas(600, 600);
-  background(50);
+  gravity = createVector(0, 0.1);
+  wind = createVector(0.01, 0.01);
+
 }
 
 function draw() {
   background(50);
+  //frameRate(5);
+
+  // create the flakes
   if(snowFlakes.length <= 200) {
     snowFlakes.push(new Flake());
   }
 
+  // update each flake and render
   for(flake of snowFlakes) {
+    flake.applyForce(gravity);
+    //flake.applyForce(wind);
+    flake.update();
     flake.render();
   }
+
+
+  
 
 }
